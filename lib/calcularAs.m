582f24d5926801b38ec4e77cdfccb96ca24a8149
Pablo Báez R <pbaez@ug.uchile.cl>
2019-06-03 18:38:39 -0400

    Se simplifica el cálculo en la zona de transición

--- lib/calcularAs.m
+++ lib/calcularAs.m
@@ -1,7 +1,7 @@
 function calcularAs(handles)
-% Esta función determina la armadura dada la geometría y esfuerzos en el elemento
+% Esta funcion determina la armadura dada la geometria y esfuerzos en el elemento
 
-% Licenciado bajos los términos del MIT.
+% Licenciado bajos los terminos del MIT.
 % Copyright (c) 2019 Pablo Baez R.
 
 handles.figure1.Pointer = 'watch';
@@ -9,10 +9,10 @@ handles.figure1.Pointer = 'watch';
 % borrar resultados antiguos
 borrarResultadosAs(handles)
 
-% verificar que los inputs sean válidos
+% verificar que los inputs sean validos
 if handles.datosValidos    
     % obtener valores almacenados de las variables
-    opcion = handles.uibuttongroup1.UserData; % 1, 2, 3, 4 ó 5
+    opcion = handles.uibuttongroup1.UserData; % 1, 2, 3, 4 o 5
     fc = handles.input_fc.UserData;
     fy = handles.input_fy.UserData;
     gamma = handles.input_gamma.UserData;
@@ -28,8 +28,8 @@ if handles.datosValidos
     if ~isempty(b) && ~isempty(h) && ~isempty(Mu) && ~isempty(Pu)        
         axes(handles.axes1)
         
-        % si la carga axial es negativa (tracción), trazar nuevamente los diagramas para incluir la rama con P<0
-        % (por defecto solo se incluye la flexo-compresión)
+        % si la carga axial es negativa (traccion), trazar nuevamente los diagramas para incluir la rama con P<0
+        % (por defecto solo se incluye la flexo-compresion)
         incluirTraccion = false;
         ylims = ylim;
         if Pu<0            
@@ -43,16 +43,16 @@ if handles.datosValidos
             ylim([0 ylims(2)])
         end 
         
-        % transformar Mu y Pu a variables adimensionales e incorporar al gráfico
+        % transformar Mu y Pu a variables adimensionales e incorporar al grafico
         xu = 10^6*Mu/(fc*b*h^2);
         yu = 10^3*Pu/(fc*b*h);        
         plot(xu,yu,'o','markerfacecolor','r','tag','punto')
         
-        % determinar cual es la curva más cercana al par (xu,yu)
+        % determinar cual es la curva mas cercana al par (xu,yu)
         x = cell(8,1);
         y = cell(8,1);
         dist = zeros(1,8);
-        for i = 1:8 % cuantías de referencia de 1 a 8%
+        for i = 1:8 % cuantias de referencia de 1 a 8%
             curvas = findobj(handles.axes1,'userdata',i);
             x{i} = curvas.XData;
             y{i} = curvas.YData;
@@ -60,11 +60,11 @@ if handles.datosValidos
         end
         [~,ind1] = min(dist);
 
-        l = h; % largo de referencia para el cálculo de la cuantía de acero
+        l = h; % largo de referencia para el calculo de la cuantia de acero
         if opcion == 4, l = h*(1-gamma); end % para muros con elementos de borde
         
-        % determinar la cuantía y área requerida de acero para las dimensiones y esfuerzos especificados
-        % valor que indica si el par (xu,yu) está bajo la curva definida por ind1
+        % determinar la cuantia y area requerida de acero para las dimensiones y esfuerzos especificados
+        % valor que indica si el par (xu,yu) esta bajo la curva definida por ind1
         flag = inpolygon(xu,yu,[0 x{ind1} 0],[0 y{ind1} 0]);
         if ind1 == 1 && flag % no calcular si rho es menor a 1%
             handles.output_rho.setText('< 1.00');
@@ -82,14 +82,14 @@ if handles.datosValidos
             lim_phi_065 = findobj(handles.axes1,'tag','lim_phi_065');
             if inpolygon(xu,yu,[0 lim_phi_09.XData xmax xmax 0],...
                     [lim_phi_09.YData(1) lim_phi_09.YData lim_phi_09.YData(end) ymin ymin])
-                caso = 1; % control por tracción
+                caso = 1; % control por traccion
                 phi = 0.9;
             elseif inpolygon(xu,yu,[0 lim_phi_065.XData xmax xmax 0],...
                     [lim_phi_065.YData(1) lim_phi_065.YData lim_phi_065.YData(end) ymin ymin])
-                caso = 2; % zona de transición (0.65 < phi < 0.9)
+                caso = 2; % zona de transicion (0.65 < phi < 0.9)
                 phi = 0.9:-0.005:0.65;
             else
-                caso = 3; % control por compresión
+                caso = 3; % control por compresion
                 phi = 0.65;
             end            
             
@@ -101,47 +101,57 @@ if handles.datosValidos
                 % definir cargas axiales nominales para calcular los diagramas PM                
                 Pitera = Pu./phi;
                 
-                % para phi = 0.65 la función 'interaccionPM2' puede requerir 
-                % un mayor número de cargas de muestra para arrojar resultados
+                % para phi = 0.65 la funcion 'interaccionPM2' puede requerir 
+                % un mayor numero de cargas de muestra para arrojar resultados
                 if caso == 3, Pitera = linspace(0,Pu/phi,10); end
                 
                 ind = length(Pitera);
                 error = tol+1;
-                rho = 0.01*ind1; % se itera a partir de la cuantía asociada a la curva más cercana al par (xu,yu)
-                dAs = 0; % diferencial de área de acero para el inicio de la iteración
+                rho = 0.01*ind1; % se itera a partir de la cuantia asociada a la curva mas cercana al par (xu,yu)
+                dAs = 0; % diferencial de area de acero para el inicio de la iteracion
                 
-                % se usa el mismo error definido para el análisis seccional
-                % (aunque aquel esté asociado a la sumatoria de cargas axiales)
+                % se usa el mismo error definido para el analisis seccional
+                % (aunque aquel este asociado a la sumatoria de cargas axiales)
                 while abs(error) > tol
                     rho = rho+dAs/(b*l);
                     [As,ys] = distribuirAs(b,h,opcion,gamma,rho,rho_w);
                     [M,P,phi_red] = interaccionPM2(b,h,As,ys,parametros,n,tol,false,Pitera);
                     
-                    % determinar el índice para el cual Pu = phi*P
+                    % determinar el indice para el cual Pu = phi*P
                     if caso == 2, [~,ind] = min(abs(Pu-phi_red.*P)); end
                     
                     % si el vector de momentos M fue redimensionado (al no haber convergencia para ciertas cargas con la
-                    % función 'interaccionPM2') probar con un vector de cargas más "tupido" y reiniciar las iteraciones
+                    % funcion 'interaccionPM2') probar con un vector de cargas mas "tupido" y reiniciar las iteraciones
                     if ind > length(M)
-                        if caso == 3, Pitera = linspace(0,Pu/phi,length(Pitera)+10); end
+                        Pitera = linspace(0,Pu/phi,length(Pitera)+10);
                         rho = 0.01*ind1;
                         dAs = 0;
                         ind = length(Pitera);
                         continue
                     end
                     
-                    % calcular el error asociado al momento y el diferencial de área para la nueva iteración
+                    % calcular el error asociado al momento y el diferencial de area para la nueva iteracion
+                    error0 = error; % almacenar error previo para fines comparativos
                     error = Mu-phi_red(ind)*M(ind); % error en kN*m
-                    dAs = 10^6*error/(fy*0.9*h); % diferencial de área en mm^2
+                    
+                    % si la solucion esta en la zona de transicion, con lo que phi no es conocido a priori, puede no
+                    % haber convergencia si el phi "exacto" no es muy cercano a alguno del rango de prueba
+                    if caso == 2 && abs(error)>abs(error0) && error0 ~= tol+1
+                        rho = rho-dAs/(b*l);
+                        [As,ys] = distribuirAs(b,h,opcion,gamma,rho,rho_w);
+                        break 
+                    else
+                        dAs = 10^6*error/(fy*0.9*h); % diferencial de area en mm^2
+                    end                    
                 end
                 
-                % calcular y trazar el diagrama para la cuantía y As previamente determinadas
+                % calcular y trazar el diagrama para la cuantia y As previamente determinadas
                 [M,P,phi_red] = interaccionPM2(b,h,As,ys,parametros,n,tol,incluirTraccion);
                 xobj = 10^6*phi_red.*M/(fc*b*h^2);
                 yobj = 10^3*phi_red.*P/(fc*b*h);
                 
-                % si el elemento es una columna, identificar el límite superior de
-                % compresión por concepto de la excentricidad accidental
+                % si el elemento es una columna, identificar el limite superior de
+                % compresion por concepto de la excentricidad accidental
                 if handles.radiobutton1.Value == 1
                     Pnmax = (0.85*fc*(b*h-sum(As))+fy*sum(As))*10^-3;
                     [~,ind08Pmax] = min(abs(P-0.8*Pnmax));                        
@@ -162,7 +172,7 @@ if handles.datosValidos
                 plot([xobj(1:ind08Pmax) 0],[yobj(1:ind08Pmax) yobj(ind08Pmax)],'r','tag','solucion')
             end
 
-            % actualización de outputs (objetos Java de tipo JLabels)
+            % actualizacion de outputs (objetos Java de tipo JLabels)
             handles.output_rho.setText(num2str(rho*100,'%.2f'));
             handles.output_As.setText(num2str(rho*b*l*0.01,'%.2f'));
         end
