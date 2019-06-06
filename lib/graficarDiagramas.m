d193dc2f167aaecdfdcb1d00d9a54895d52bf3b4
Pablo Báez R <pbaez@ug.uchile.cl>
2019-06-06 19:57:13 -0400

    Se agrega opción de columna circular
    Además se eliminan los tildes en los comentarios (es un parto escribir así, pero bueh...)

--- lib/graficarDiagramas.m
+++ lib/graficarDiagramas.m
@@ -1,7 +1,7 @@
 function graficarDiagramas(handles,varargin)
-% Esta función grafica los diagramas para las cuantías de refuerzo de 1 a 8%
+% Esta funcion grafica los diagramas para las cuantias de refuerzo de 1 a 8%
 
-% Licenciado bajos los términos del MIT.
+% Licenciado bajos los terminos del MIT.
 % Copyright (c) 2019 Pablo Baez R.
 
 handles.figure1.Pointer = 'watch';
@@ -11,13 +11,13 @@ axes(handles.axes1), cla
 handles.output_rho.setText('');
 handles.output_As.setText('');
 
-% definir valores arbitrarios para la sección
-% (no influyen en el cálculo pues M y P son normalizados por el área de la sección)
-b = 100; % ancho de la sección
-h = 100; % altura de la sección
+% definir valores arbitrarios para la seccion
+% (no influyen en el calculo pues M y P son normalizados por el area de la seccion)
+b = 100; % ancho de la seccion
+h = 100; % altura de la seccion
 
 % obtener valores de los inputs
-gamma = handles.input_gamma.UserData; % ubicación de la armadura As2 (en mm)
+gamma = handles.input_gamma.UserData; % ubicacion de la armadura As2 (en mm)
 rho_w = handles.input_rho_w.UserData;
 n = handles.input_n.UserData;
 tol = handles.input_error.UserData;
@@ -26,12 +26,12 @@ fc = parametros.fc;
 fy = parametros.fy;
 
 if ~isempty(n) && ~isempty(tol)
-    opcion=handles.uibuttongroup1.UserData;    
+    opcion = handles.uibuttongroup1.UserData;    
     
     grid on, hold on
     set(handles.axes1,'xlimmode','auto','ylimmode','auto')
     
-    % verificar la congruencia de los inputs asociados a la distribución de la armadura
+    % verificar la congruencia de los inputs asociados a la distribucion de la armadura
     if (opcion == 4 && (isempty(gamma) || isempty(rho_w))) || (opcion ~= 5 && isempty(gamma))
         handles.figure1.Pointer = 'arrow';
         handles.datosValidos = false;
@@ -44,35 +44,45 @@ if ~isempty(n) && ~isempty(tol)
     incluirTraccion = false;
     if ~isempty(varargin), incluirTraccion = varargin{1}; end
     
-    text(0.96,0.95,handles.ecuacion_cuantia{opcion},'units','normalized','interpreter','latex',...
-        'horizontalalignment','right','verticalalignment','top','edgecolor','k','backgroundcolor','w')
+    ecuacion = text(0.96,0.95,handles.ecuacion_cuantia{opcion},'units','normalized','interpreter','latex',...
+        'horizontalalignment','right','verticalalignment','top','edgecolor','k','backgroundcolor','w');
     
-    % inicializar variables que que se usarán para identificar zonas de interés en el diagrama
+    % inicializar variables que que se usaran para identificar zonas de interes en el diagrama
     ymax = zeros(1,8);
     xlim08Pmax = zeros(1,8);
     xyLim_et0 = zeros(8,2);
     xyLim_et_ey = zeros(8,2);
     xyLim_et0005 = zeros(8,2);
     
-    % graficar los diagramas para las cuantías de 1 a 8%
+    % determinar si la seccion es circular
+    Ag = b*h; % area gruesa de la seccion
+    factor = 0.8; % factor de reducion de carga maxima por excentricidad accidental para columnas con estribos
+    if opcion == 3 && handles.uibuttongroup4.UserData == 2 % columna circular
+        opcion = 3.5;
+        Ag = 0.25*pi*h^2;
+        factor = 0.85; % se supone que se usan espirales para confinar la armadura longitudinal
+        ecuacion.String = '$$\rho_g = \frac{A_s}{A_g} = \frac{A_s}{\pi(h/2)^2}$$';
+    end
+    
+    % graficar los diagramas para las cuantias de 1 a 8%
     for i=1:8
-        % determinar la distribución del refuerzo por capas
+        % determinar la distribucion del refuerzo por capas
         [As,ys] = distribuirAs(b,h,opcion,gamma,i*0.01,rho_w);
         
-        % determinación de resultados para los inputs considerados y la cuantía correspondiente
-        [M,P,phi_red,et] = interaccionPM2(b,h,As,ys,parametros,n,tol,incluirTraccion);
+        % determinacion de resultados para los inputs considerados y la cuantia correspondiente
+        [M,P,phi_red,et] = interaccionPM2(b,h,As,ys,parametros,n,tol,incluirTraccion,opcion == 3.5);
         
         % transformar M y P a variables adimensionales
-        x = phi_red.*M*10^6/(fc*b*h^2);
-        y = phi_red.*P*10^3/(fc*b*h);
+        x = phi_red.*M*10^6/(fc*Ag*h);
+        y = phi_red.*P*10^3/(fc*Ag);
         
         ymax(i) = y(end);
         
-        % incorporar el límite de compresión máxima si el elemento se trata de una columna
+        % incorporar el limite de compresion maxima si el elemento se trata de una columna
         if handles.radiobutton1.Value == 1
-            % identificar el límite superior de compresión por concepto de la excentricidad accidental
-            Pmax = (0.85*fc*(b*h-sum(As))+fy*sum(As))*10^-3;
-            [~,ind] = min(abs(P-0.8*Pmax));
+            % identificar el limite superior de compresion por concepto de la excentricidad accidental
+            Pmax = (0.85*fc*(Ag-sum(As))+fy*sum(As))*10^-3;
+            [~,ind] = min(abs(P-factor*Pmax));
             ymax(i) = y(ind);
             xlim08Pmax(i) = x(ind);
 
@@ -83,9 +93,9 @@ if ~isempty(n) && ~isempty(tol)
             plot(x,y,'color','b','userdata',i)
         end
         
-        % determinar zonás límites en el diagrama
-        [~,ind_fs0] = min(abs(et)); % tensión nula en la capa más cercana a la fibra más traccionada
-        [~,ind_inicio_fluencia] = min(abs(-et-fy/200000)); % et es negativa para tracción
+        % determinar zonas limites en el diagrama
+        [~,ind_fs0] = min(abs(et)); % tension nula en la capa mas cercana a la fibra mas traccionada
+        [~,ind_inicio_fluencia] = min(abs(-et-fy/200000)); % et es negativa para traccion
         [~,ind_et0005] = min(abs(-et-0.005));        
         
         xyLim_et0(i,:) = [x(ind_fs0) y(ind_fs0)];
@@ -93,8 +103,8 @@ if ~isempty(n) && ~isempty(tol)
         xyLim_et0005(i,:) = [x(ind_et0005) y(ind_et0005)];
     end
     
-    % mostrar las zonas límites en que fs = 0 y fs = fy para el acero en tracción 
-    % (se deja oculto el límite en et > 0.005 i.e phi = 0.9)
+    % mostrar las zonas limites en que fs = 0 y fs = fy para el acero en traccion 
+    % (se deja oculto el limite en et > 0.005 i.e phi = 0.9)
     plot(xyLim_et0(:,1),xyLim_et0(:,2),'--','color','k')
     plot(xyLim_et_ey(:,1),xyLim_et_ey(:,2),'--','color','k','tag','lim_phi_065')
     plot(xyLim_et0005(:,1),xyLim_et0005(:,2),'--','color','k','tag','lim_phi_09','visible','off')
@@ -103,8 +113,8 @@ if ~isempty(n) && ~isempty(tol)
     text(xyLim_et0(end,1),xyLim_et0(end,2),'f_s = 0',tipoTexto{:});
     text(xyLim_et_ey(end,1),xyLim_et_ey(end,2),'f_s = f_y',tipoTexto{:});
     
-    % mostrar textos indicando las cuantías de acero extremas del diagrama
-    % (1 y 8% del área gruesa de la sección completa o del elemento de borde según corresponda)
+    % mostrar textos indicando las cuantias de acero extremas del diagrama
+    % (1 y 8% del area gruesa de la seccion completa o del elemento de borde segun corresponda)
     if handles.radiobutton1.Value == 1        
         handles.ylim_08Pmax = ymax;
         handles.xlim_08Pmax = xlim08Pmax;
